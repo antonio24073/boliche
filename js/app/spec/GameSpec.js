f97c05d52e5c3e7995cdb61726db41067001aa22
Antonio Augusto D Agostino <antonio.etec.inf5@gmail.com>
2019-03-24 14:52:42 -0300

    primeiro teste

--- js/app/spec/GameSpec.js
+++ js/app/spec/GameSpec.js
@@ -1,12 +1,31 @@
 describe("Game", function() {
     var jogada;
+    var game;
+
 
     beforeEach(function() {
-        jogada = new Jogada(3);
+        game = new Game();
+        let jogador1 = new Jogador();
+        let listFrames1 = new ListFrames(jogador1);
+        game.adiciona(listFrames1);
+        let jogador2 = new Jogador();
+        let listFrames2 = new ListFrames(jogador2);
+        game.adiciona(listFrames2);
+
     });
 
-    it("should be able to play a Song", function() {
-        jogada.play();
-        expect(jogada.pinos).toEqual(3);
+    it("deveria ser capaz de calcular pontos", function() {
+        let frame1 = new Frame();
+        frame1.adiciona(new Jogada(3));
+        frame1.adiciona(new Jogada(2));
+        game.listFramesAtual.adiciona(frame1);
+        game.passaVez();
+        let frame2 = new Frame();
+        frame2.adiciona(new Jogada(0));
+        frame2.adiciona(new Jogada(10));
+        game.listFramesAtual.adiciona(frame2);
+
+        expect(game.listFrames[0].frames[0].pontos).toEqual(5);
+        expect(game.listFrames[1].frames[0].pontos).toEqual(10);
     });
 });
