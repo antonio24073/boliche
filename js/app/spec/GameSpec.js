26cd2057b1b7f094393ce32b54797f6e4bdeb409
Antonio Augusto D Agostino <antonio.etec.inf5@gmail.com>
2019-03-24 15:06:19 -0300

    Testes faz strike, faz spare

--- js/app/spec/GameSpec.js
+++ js/app/spec/GameSpec.js
@@ -14,18 +14,57 @@ describe("Game", function() {
 
     });
 
-    it("deveria ser capaz de calcular pontos", function() {
+    it("Calcula pontos", function() {
         let frame1 = new Frame();
         frame1.adiciona(new Jogada(3));
         frame1.adiciona(new Jogada(2));
         game.listFramesAtual.adiciona(frame1);
         game.passaVez();
         let frame2 = new Frame();
-        frame2.adiciona(new Jogada(0));
-        frame2.adiciona(new Jogada(10));
+        frame2.adiciona(new Jogada(5));
+        frame2.adiciona(new Jogada(5));
         game.listFramesAtual.adiciona(frame2);
 
-        expect(game.listFrames[0].frames[0].pontos).toEqual(5);
-        expect(game.listFrames[1].frames[0].pontos).toEqual(10);
+        expect(5).toEqual(game.listFrames[0].frames[0].pontos);
+        expect(10).toEqual(game.listFrames[1].frames[0].pontos);
+    });
+    it("Faz Strike", function() {
+        let frame1 = new Frame();
+        frame1.adiciona(new Jogada(10));
+        game.listFramesAtual.adiciona(frame1);
+        game.passaVez();
+        let frame2 = new Frame();
+        frame2.adiciona(new Jogada(5));
+        frame2.adiciona(new Jogada(3));
+        game.listFramesAtual.adiciona(frame2);
+        game.passaVez();
+        let frame3 = new Frame();
+        frame3.adiciona(new Jogada(2));
+        frame3.adiciona(new Jogada(4));
+        game.listFramesAtual.adiciona(frame3);
+        game.passaVez();
+
+        expect(16).toEqual(game.listFrames[0].frames[0].pontos);
+    });
+    it("Faz Spare", function() {
+        let frame1 = new Frame();
+        frame1.adiciona(new Jogada(5));
+        frame1.adiciona(new Jogada(5));
+        game.listFramesAtual.adiciona(frame1);
+        game.passaVez();
+        let frame2 = new Frame();
+        frame2.adiciona(new Jogada(5));
+        frame2.adiciona(new Jogada(3));
+        game.listFramesAtual.adiciona(frame2);
+        game.passaVez();
+        let frame3 = new Frame();
+        frame3.adiciona(new Jogada(2));
+        frame3.adiciona(new Jogada(4));
+        game.listFramesAtual.adiciona(frame3);
+        game.passaVez();
+
+        expect(12).toEqual(game.listFrames[0].frames[0].pontos);
+    });
+    it("E assim por diante ...", function() {
     });
 });
